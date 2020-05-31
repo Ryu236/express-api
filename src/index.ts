45f20ed44736a9da56a594bb4737032b012ea6ee
Ryu236 <ryu.adv.2360@gmail.com>
2020-05-31 17:36:27 +0900

    :sparkles: Feat(router): add cors, router and post method

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,31 @@
 import express from 'express'
 
 const app = express()
-app.get('/', (req, res) => {
+
+// CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const router = express.Router()
+
+router.post('/api/test', (req, res) => {
+  res.send(req.body)
+})
+
+router.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello World!')
 })
+
+app.use(router)
 
 app.listen(3000, () => {
   console.log('Running on http://localhost:3000')
